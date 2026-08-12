5ed6de6d31406293f1542a879f4e2a807cc57bec
Moumen Awad <moumen.awad.cs@gmail.com>
2026-08-12 19:04:12 +0200

    - Remove old root directories and redundant files

--- warehouse_dbt/models/marts/daily_coin_metrics.sql
+++
@@ -1,9 +0,0 @@
-select
-    coin_id, 
-    name, 
-    date_trunc('day', loaded_at) as metric_date,
-    avg (current_price_usd) as avg_price_usd,
-    max (market_cap_usd) as market_cap_usd,
-    avg (price_change_pct_24h) as avg_price_change_pct_24h
-from {{ref('stg_coins_markets')}}
-group by coin_id, name, date_trunc('day', loaded_at)
